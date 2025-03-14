fd0ca500ad86c15e139506f932c92b6069cafed1
R1nDzin <88189403+R1nDzin@users.noreply.github.com>
2025-03-14 21:39:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,51 @@
 import telebot
 import requests
-from bs4 import BeautifulSoup
 
 TOKEN = "7606824571:AAHqA-s6DxIAX1QCZ214N3R36b37dNyvoTc"
+WEATHER_API = "69fa2cc731deb0ffe06c9456ef9a2074"
 bot = telebot.TeleBot(TOKEN)
 
 cities = {
-    "Москва": "moscow-4368",
-    "Санкт-Петербург": "saint-petersburg-4079",
-    "Казань": "kazan-4364"
+    "Москва": "Moscow",
+    "Санкт-Петербург": "Saint Petersburg",
+    "Казань": "Kazan"
 }
 
-# Функция получения температуры с Gismeteo
-def get_temp(city_url):
-    url = f"https://www.gismeteo.ru/weather-{city_url}/now/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    temp_span = soup.find('span', class_='unit_temperature_c')
-    if temp_span:
-        return int(temp_span.text.replace('−', '-').replace('°', '').strip())
-    else:
+def get_temp(city_name):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={WEATHER_API}&units=metric&lang=ru"
+    response = requests.get(url).json()
+    try:
+        temp = response.json()["main"]["temp"]
+        return temp
+    except:
         return None
 
 def get_advice(temp):
     if temp <= 0:
-        return "🥶 Очень холодно, надевай шапку и куртку!"
+        return "Очень холодно, надевай шапку и куртку! 🥶"
     elif temp <= 10:
-        return "Прохладно, нужна куртка 🧥."
+        return "Прохладно, нужна куртка 🧥"
     elif temp <= 20:
-        return "Комфортно, подойдет легкая куртка или кофта! 👕"
+        return "Комфортная погода, надень кофту или лёгкую куртку 👕"
     else:
-        return "Тепло, футболка и шорты подойдут отлично ☀️"
+        return "Тепло! Отлично подойдёт футболка и шорты ☀️"
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(*cities.keys())
     bot.send_message(message.chat.id, "Выбери город:", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: message.text in cities)
-def send_weather(message):
-    city = message.text
-    city_url = cities[city]
-    temp = get_temp(city_url)
-    
+def weather(message):
+    city_name = message.text
+    city_query = cities[city_name]
+    temp = get_temp(city=city_name)
+
     if temp is not None:
         advice = get_advice(temp)
-        bot.send_message(message.chat.id, f"🌡 Погода в {city} сейчас {temp}°C.\n{advice}")
+        bot.send_message(message.chat.id, f"🌡 В городе {city_name} сейчас {temp}°C.\n{advice}")
     else:
-        bot.send_message(message.chat.id, "Не удалось получить погоду. Попробуй позже.")
+        bot.send_message(message.chat.id, "Не удалось получить погоду, попробуй позже.")
 
 bot.polling()
